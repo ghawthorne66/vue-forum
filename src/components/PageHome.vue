78a44794d7496b4f6067374fba087baf50b4ebab
Greg Hawthorne <ghawthorne@me.com>
2021-08-29 20:07:25 +0200

    Create ThreadList.vue and Clean up the Homepage

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Welcome to the forum</h1>
-  <ThreadList />
+  <ThreadList :threads="threads"/>
 </template>
 
 <script>
@@ -14,18 +14,7 @@ export default {
   data() {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users,
     }
-  },
-  methods: {
-    postById(postId) {
-      return this.posts.find(p => p.id === postId)
-    },
-    userById(userId) {
-      return this.users.find(p => p.id === userId)
-    }
-
   },
   name: "pageHome"
 }
